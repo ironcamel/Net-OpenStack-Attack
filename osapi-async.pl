a22db628cca17dcca6f4d9d03d4cbb7d2862b816
William Wolf <wwolf@siteworx.com>
2011-06-22 11:44:38 -0400

    use App::Rad

--- osapi-async.pl
+++ osapi-async.pl
@@ -5,11 +5,11 @@ use warnings;
 use Data::Dumper;
 use HTTP::Async;
 use HTTP::Request;
+use App::Rad;
 use LWP;
-use JSON qw(from_json to_json);
+use JSON qw(to_json from_json);
 
 my ($action, $num_runs) = @ARGV;
-die "action argument required, e.g., create_servers\n" unless $action;
 $num_runs ||= 1;
 
 my $base_url = $ENV{NOVA_URL};
@@ -31,6 +31,9 @@ my $auth_headers = [
     "content-type" => "application/json"
 ];
 
+
+App::Rad->run();
+
 ########################
 # CREATE BODY JSON
 ########################
@@ -42,119 +45,125 @@ my $create_body = to_json({
     }
 });
 
-my $async = HTTP::Async->new;
+sub create_servers 
+:Help(create x number of servers)
+{
+    my ($successes, $failures, @errmsgs) = (0, 0);
+    my $async = HTTP::Async->new;
 
-given ($action){
-    ########################
-    # CREATE SERVERS
-    ########################
-    when ("create_servers") {
-        my ($successes, $failures, @errmsgs) = (0, 0);
-        say "Creating $num_runs servers...";
-        for my $i (1 .. $num_runs) {
-            my $req = HTTP::Request->new(
-                POST => "$base_url/servers", $auth_headers, $create_body);
-            $async->add($req);
-        }
-        while (my $res = $async->wait_for_next_response) {
-            if ($res->status_line =~ /^2/){
-                $successes++;
-            } else {
-                $failures++;
-                push @errmsgs, $res->content;
-            }
-        }
-        say "Successes: $successes Failures: $failures.";
-        say Dumper(\@errmsgs) if @errmsgs;
+    say "Creating $num_runs servers...";
+    for my $i (1 .. $num_runs) {
+        my $req = HTTP::Request->new(
+            POST => "$base_url/servers", $auth_headers, $create_body);
+        $async->add($req);
     }
-    ########################
-    # DELETE SERVERS
-    ########################
-    when ("delete_servers") {
-        my ($successes, $failures, @errmsgs) = (0, 0);
-        my $res = $ua->get("$base_url/servers", $auth_headers);
-        die "Error getting server list" unless $res->status_line =~ /^2/;
-        my $data = from_json($res->content);
-        my @servers = @{ $data->{servers} };
-        my $num_servers = @servers;
-        say "Deleting $num_servers servers...";
-        foreach my $server (@servers){
-            my $id = $server->{id};
-            my $req = HTTP::Request->new(
-                DELETE => "$base_url/servers/$id", $auth_headers);
-            $async->add($req);
-            while (my $res = $async->wait_for_next_response) {
-                if ($res->status_line =~ /^2/) {
-                    $successes++;
-                } else {
-                    $failures++;
-                    push @errmsgs, $res->content;
-                }
-            }
+    while (my $res = $async->wait_for_next_response) {
+        if ($res->status_line =~ /^2/){
+            $successes++;
+        } else {
+            $failures++;
+            push @errmsgs, $res->content;
         }
-
-        say "Successes: $successes Failures: $failures";
-        say Dumper(\@errmsgs) if @errmsgs;
     }
-    when ("invalid") {
-        my ($successes, $failures, @errmsgs) = (0, 0);
-        say "Sending $num_runs invalid requests...";
-        for my $i (1 .. $num_runs) {
-            my $req = HTTP::Request->new(
-                GET => "$base_url/invalid-resource", $auth_headers, $create_body);
-            $async->add($req);
-        }
-        while (my $res = $async->wait_for_next_response) {
-            if ($res->status_line =~ /^2/){
-                $successes++;
-            } else {
-                $failures++;
-                push @errmsgs, $res->content;
-            }
-        }
-        say "Successes: $successes Failures: $failures.";
-        #say Dumper(\@errmsgs) if @errmsgs;
-    }
-    when ("images") {
-        my ($successes, $failures, @errmsgs) = (0, 0);
-        say "Sending $num_runs /images requests...";
-        for my $i (1 .. $num_runs) {
-            my $req = HTTP::Request->new(
-                GET => "$base_url/images", $auth_headers, $create_body);
-            $async->add($req);
-        }
-        while (my $res = $async->wait_for_next_response) {
-            if ($res->status_line =~ /^2/){
-                $successes++;
-            } else {
-                $failures++;
-                push @errmsgs, $res->content;
-            }
-        }
-        say "Successes: $successes Failures: $failures.";
-        #say Dumper(\@errmsgs) if @errmsgs;
-    }
-    when ("servers") {
-        my ($successes, $failures, @errmsgs) = (0, 0);
-        say "Sending $num_runs /servers requests...";
-        for my $i (1 .. $num_runs) {
-            my $req = HTTP::Request->new(
-                GET => "$base_url/servers", $auth_headers, $create_body);
-            $async->add($req);
-        }
-        while (my $res = $async->wait_for_next_response) {
-            if ($res->status_line =~ /^2/){
-                $successes++;
-            } else {
-                $failures++;
-                push @errmsgs, $res->content;
-            }
-        }
-        say "Successes: $successes Failures: $failures.";
-        #say Dumper(\@errmsgs) if @errmsgs;
-    }
-    default {
-        say "unknown command $_";
-    }
+    say "Successes: $successes Failures: $failures.";
+    #say Dumper(\@errmsgs) if @errmsgs;
 }
 
+sub delete_servers 
+:Help(delete all servers)
+{
+    my ($successes, $failures, @errmsgs) = (0, 0);
+    my $async = HTTP::Async->new;
+    my $res = $ua->get("$base_url/servers", $auth_headers);
+    die "Error getting server list" unless $res->status_line =~ /^2/;
+    my $data = from_json($res->content);
+    my @servers = @{ $data->{servers} };
+    my $num_servers = @servers;
+    say "Deleting $num_servers servers...";
+    foreach my $server (@servers){
+        my $id = $server->{id};
+        my $req = HTTP::Request->new(
+            DELETE => "$base_url/servers/$id", $auth_headers);
+        $async->add($req);
+        while (my $res = $async->wait_for_next_response) {
+            if ($res->status_line =~ /^2/) {
+                $successes++;
+            } else {
+                $failures++;
+                push @errmsgs, $res->content;
+            }
+        }
+    }
+
+    say "Successes: $successes Failures: $failures";
+    #say Dumper(\@errmsgs) if @errmsgs;
+}
+
+sub invalid 
+:Help(run invalid request x number of times)
+{
+    my ($successes, $failures, @errmsgs) = (0, 0);
+    my $async = HTTP::Async->new;
+    say "Sending $num_runs invalid requests...";
+    for my $i (1 .. $num_runs) {
+        my $req = HTTP::Request->new(
+            GET => "$base_url/invalid-resource", $auth_headers, $create_body);
+        $async->add($req);
+    }
+    while (my $res = $async->wait_for_next_response) {
+        if ($res->status_line =~ /^2/){
+            $successes++;
+        } else {
+            $failures++;
+            push @errmsgs, $res->content;
+        }
+    }
+    say "Successes: $successes Failures: $failures.";
+    #say Dumper(\@errmsgs) if @errmsgs;
+}
+
+sub images
+:Help(run images request x number of times)
+{
+    my ($successes, $failures, @errmsgs) = (0, 0);
+    my $async = HTTP::Async->new;
+    say "Sending $num_runs /images requests...";
+    for my $i (1 .. $num_runs) {
+        my $req = HTTP::Request->new(
+            GET => "$base_url/images", $auth_headers, $create_body);
+        $async->add($req);
+    }
+    while (my $res = $async->wait_for_next_response) {
+        if ($res->status_line =~ /^2/){
+            $successes++;
+        } else {
+            $failures++;
+            push @errmsgs, $res->content;
+        }
+    }
+    say "Successes: $successes Failures: $failures.";
+    #say Dumper(\@errmsgs) if @errmsgs;
+}
+
+sub servers 
+:Help(run server list request x number of times)
+{
+    my ($successes, $failures, @errmsgs) = (0, 0);
+    my $async = HTTP::Async->new;
+    say "Sending $num_runs /servers requests...";
+    for my $i (1 .. $num_runs) {
+        my $req = HTTP::Request->new(
+            GET => "$base_url/servers", $auth_headers, $create_body);
+        $async->add($req);
+    }
+    while (my $res = $async->wait_for_next_response) {
+        if ($res->status_line =~ /^2/){
+            $successes++;
+        } else {
+            $failures++;
+            push @errmsgs, $res->content;
+        }
+    }
+    say "Successes: $successes Failures: $failures.";
+    #say Dumper(\@errmsgs) if @errmsgs;
+}
