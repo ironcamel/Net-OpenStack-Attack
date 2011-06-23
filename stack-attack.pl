5b1b525487e66510c1fa3ca3794f0d4c1172dd16
Naveed Massjouni <naveedm9@gmail.com>
2011-06-23 01:47:40 -0400

    Code deduplication.

--- stack-attack.pl
+++ stack-attack.pl
@@ -10,14 +10,16 @@ use JSON qw(to_json from_json);
 
 sub setup {
     my $c = shift;
+
     $c->register_commands({
         create_servers => 'create x number of servers',
         delete_servers => 'delete all servers',
         servers        => 'run x number of server list requests',
-        bad            => 'run x number of bad/invalid requests',
         images         => 'run x number of image list requests',
+        bad            => 'run x number of bad/invalid requests',
     });
 
+    # Construct the base url
     my $base_url = $ENV{NOVA_URL};
     die "NOVA_URL env var is missing. Did you forget to source novarc?\n"
         unless $base_url;
@@ -25,38 +27,22 @@ sub setup {
     $base_url =~ s/v1\.0/v1.1/; # Switch to version 1.1
     $c->stash->{base_url} = $base_url;
 
-    # Get the Auth Token
+    # Save the auth token
     my $ua = LWP::UserAgent->new();
     my $res = $ua->get(
         $base_url,
         'x-auth-key'  => $ENV{NOVA_API_KEY},
         'x-auth-user' => $ENV{NOVA_USERNAME},
     );  
-
-    # Store auth_headers
     $c->stash->{auth_headers} = [
         "x-auth-token" => $res->header('x-auth-token'),
         "content-type" => "application/json"
     ];
-
-    # Create body json
-    $c->stash->{create_body_json} = to_json({
-        server => {
-            name      => 'test-server',
-            imageRef  => '3',
-            flavorRef => '1',
-        }
-    });
 }
 
 sub pre_process {
     my $c = shift;
-    my ($num_runs) = @ARGV;
-
-    die "The delete_servers command does not accept an arguments\n"
-        if $c->cmd eq 'delete_servers' and $num_runs;
-
-    $c->stash->{num_runs} = $num_runs || 1;
+    $c->stash->{num_runs} = $ARGV[0] || 1;
 }
 
 sub post_process {
@@ -73,48 +59,38 @@ App::Rad->run();
 
 #---------- Commands ----------------------------------------------------------
 
-
 sub create_servers {
     my $c = shift;
-    my $base_url = $c->stash->{base_url};
-
     say "Creating " . $c->stash->{num_runs} . " servers...";
-    return make_requests(
-        $c->stash->{num_runs},
-        "POST", 
-        "$base_url/servers", 
-        $c->stash->{auth_headers}, 
-        $c->stash->{create_body_json}
-    );
+    return make_requests($c, POST => '/servers', to_json {
+        server => {
+            name      => 'test-server',
+            imageRef  => 3,
+            flavorRef => 1,
+        }
+    });
 }
 
 sub delete_servers {
     my $c = shift;
-    my $base_url = $c->stash->{base_url};
-    my ($successes, $failures, @errmsgs) = (0, 0);
 
-    my $async = HTTP::Async->new;
+    die "The delete_servers command does not accept any arguments\n" if @ARGV;
+
     my $ua = LWP::UserAgent->new();
+    my $base_url = $c->stash->{base_url};
+    my $res = $ua->get("$base_url/servers", @{ $c->stash->{auth_headers} });
 
-    my $res = $ua->get(
-        "$base_url/servers", 
-        'x-auth-token' => $c->stash->{auth_headers}->[1]
-    );
-    die "Error getting server list " . $res->content unless $res->status_line =~ /^2/;
+    die "Error getting server list " . $res->content
+        unless $res->status_line =~ /^2/;
 
     my $data = from_json($res->content);
     my @servers = @{ $data->{servers} };
 
     say "Deleting " . @servers . " servers...";
+    my ($successes, $failures, @errmsgs) = (0, 0);
     foreach my $server (@servers){
         my $id = $server->{id};
-        
-        my $reval = make_requests(
-            1,
-            "DELETE", 
-            "$base_url/servers/$id", 
-            $c->stash->{auth_headers}, 
-        );
+        my $reval = make_requests($c, DELETE => "/servers/$id");
         $successes += $reval->[0];
         $failures += $reval->[1];
     }
@@ -123,56 +99,37 @@ sub delete_servers {
 
 sub bad {
     my $c = shift;
-    my $base_url = $c->stash->{base_url};
-
     say "Sending " . $c->stash->{num_runs} . " invalid requests...";
-
-    return make_requests(
-        $c->stash->{num_runs},
-        "GET", 
-        "$base_url/invalid-resource", 
-        $c->stash->{auth_headers},
-    );
+    return make_requests($c, GET => '/invalid-resource');
 }
 
 sub images {
     my $c = shift;
-    my $base_url = $c->stash->{base_url};
-
-    return make_requests(
-        $c->stash->{num_runs},
-        "GET", 
-        "$base_url/images", 
-        $c->stash->{auth_headers},
-    );
+    say "Sending " . $c->stash->{num_runs} . " /images requests...";
+    return make_requests($c, GET => '/images');
 }
 
 sub servers {
     my $c = shift;
-    my $base_url = $c->stash->{base_url};
-
     say "Sending " . $c->stash->{num_runs} . " /servers requests...";
-    return make_requests(
-        $c->stash->{num_runs},
-        "GET", 
-        "$base_url/servers", 
-        $c->stash->{auth_headers},
-    );
+    return make_requests($c, GET => '/servers');
 }
 
 #---------- Helpers -----------------------------------------------------------
 
 sub make_requests {
-    my ($num_reqs, $method, $url, $headers, $body) = @_;
+    my ($c, $method, $resource, $body) = @_;
+    my $url = $c->stash->{base_url} . $resource;
+    my $headers = $c->stash->{auth_headers};
     my ($successes, $failures, @errmsgs) = (0, 0);
     my $async = HTTP::Async->new;
 
-    for my $i (1 .. $num_reqs) {
+    for my $i (1 .. $c->stash->{num_runs}) {
         my $req = HTTP::Request->new($method => $url, $headers, $body);
         $async->add($req);
     }
     while (my $res = $async->wait_for_next_response) {
-        if ($res->status_line =~ /^2/){
+        if ($res->status_line =~ /^2/) {
             $successes++;
         } else {
             $failures++;
